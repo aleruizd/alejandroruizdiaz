e24e4fdbe3f7e83db234fe2f419784ef045f67bd
Alejandro Ruiz Diaz <60278448+aleruizd@users.noreply.github.com>
2021-01-24 13:55:19 -0300

    Portfolio version 1.0

--- pages/api/message.js
+++ pages/api/message.js
@@ -12,8 +12,6 @@ export default function handler(req, res) {
 
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true, 
         auth: {
             user: `${process.env.EMAIL}`,
             pass: `${process.env.PASS}`, 
